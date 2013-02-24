94ba84a7b153045619dd3d3d36924781a587a13e
Maz <ma.mazmaz@gmail.com>
2013-02-24 14:09:10 -0500

    added build script based on uglifyjs

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+jison cpp-parser.jison && coffee cpp.coffee && \
+	cat cpp-parser.js cpp.js>>combined.js && \
+		#for some reason, piping it in doesn't work
+	uglifyjs -o cpp-min.js combined.js && \
+		rm combined.js
+exit $?
