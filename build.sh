a97e9f0882f1936863b9675095d9f69d654c8bac
Maz <ma.mazmaz@gmail.com>
2013-03-02 13:17:20 -0500

    fixed indent in build.sh

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ jison cpp-parser.jison && coffee cpp.coffee && \
 	cat cpp-parser.js cpp.js>>combined.js && \
 		#for some reason, piping it in doesn't work
 	uglifyjs -o cpp-min.js combined.js && \
-		rm combined.js
+	rm combined.js
 exit $?
